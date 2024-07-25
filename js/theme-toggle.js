b88a28a4c7c6c15bdd3bc64e261e03b676fc0a50
propeller-man <k.vetoshkin@x-run.online>
2024-07-26 00:29:43 +0300

    Update theme-toggle.js

--- js/theme-toggle.js
+++ js/theme-toggle.js
@@ -1,1 +1,19 @@
+// theme-toggle.js
+
+const toggleTheme = () => {
+    const body = document.body;
+    body.classList.toggle('dark-theme');
+
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+};
+
+// Set the theme on initial load
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+    document.body.classList.add(storedTheme);
+}
 
