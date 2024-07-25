d1f01ef8b67d0734a7a00b22327275090a148dc4
propeller-man <k.vetoshkin@x-run.online>
2024-07-26 00:30:03 +0300

    Update components.js

--- js/components.js
+++ js/components.js
@@ -1,1 +1,20 @@
+/* components.css */
+
+/* Кастомные стили для карточек */
+.card-custom {
+    border-radius: 10px;
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+/* Кастомные стили для кнопок */
+.btn-custom {
+    background-color: var(--bs-primary);
+    color: #fff;
+    border-radius: 50px;
+}
+
+/* Кастомные стили для модальных окон */
+.modal-custom .modal-content {
+    border-radius: 15px;
+}
 
